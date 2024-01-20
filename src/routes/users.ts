53a0676e2de915137ae838da64136a107eef9ea7
samuelpavanati <samuelnascimento278@gmail.com>
2024-01-19 21:41:38 -0300

    changed routes function

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,8 +1,8 @@
 import { randomUUID } from 'node:crypto'
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
 
-export async function routes(fastify: FastifyInstance) {
-	fastify.get('/', async(request: FastifyRequest, reply: FastifyReply) => {
+export async function users(fastify: FastifyInstance) {
+	fastify.get('/users', async(request: FastifyRequest, reply: FastifyReply) => {
 		reply.send({
 			id: randomUUID(),
 			name: 'John Doe',
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import { routes } from './routes/users'
+import { users } from './routes/users'
 import fastify from 'fastify'
 import fastifyCookie from '@fastify/cookie'
 
 const app = fastify()
 
 app.register(fastifyCookie)
-app.register(routes, { prefix: '/users' })
+app.register(users)
 
 const host = '0.0.0.0'
 const port = 3333

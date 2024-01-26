c431aa5a773dfb283874eb68dfa93c177df06c6a
samuelpavanati <samuelnascimento278@gmail.com>
2024-01-26 19:41:10 -0300

    read JSON is half done

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,7 +1,7 @@
 import { randomUUID } from 'node:crypto'
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
-import fs from 'fs/promises'
+import fs from 'node:fs'
 
 export async function users(app: FastifyInstance) {
 	app.get('/users', async(request: FastifyRequest, reply: FastifyReply) => {
@@ -14,8 +14,15 @@ export async function users(app: FastifyInstance) {
 	})  
 
 	app.post('/create', async(request: FastifyRequest, reply: FastifyReply) => {
+		const path = './user-data.json'
+
+		const buffer = fs.readFileSync(path, 'utf8')
+
+		console.log('buffer', buffer)
 
 		const buffers = []
+
+		buffers.push(buffer)
 
 		const registerUserSchema = z.object({
 			name: z.string(),
@@ -32,19 +39,40 @@ export async function users(app: FastifyInstance) {
 			password: password,
 		}
 
+		console.log('buffer antes', buffers)
+
 		// save to buffer
 		buffers.push(saveUserSchema)
 
+		// fs.readFile(path, 'utf-8')
+		// , (err, data) => {
+		// 	if (err) {
+		// 		console.log('Error! Cannot read the file!')
+		// 		return
+		// 	}
+
+		// 	try {
+		// 		const getData = JSON.parse(data)
+		// 		console.log('Data read from JSON file: ', getData) 
+		// 	} catch (err) {
+		// 		console.error('Error parsing JSON: ', err)
+		// 	}
+		// })
+
 		// terminal
-		console.log(buffers)
+		// console.log(buffers)
 		
 		// convert to JSON
 		const saveUserToJSON = JSON.stringify(buffers, null, 2)
 
-		const saveFile = './user-data.json'
-
 		// save to file
-		await fs.appendFile(saveFile, saveUserToJSON)
+		fs.writeFile(path, saveUserToJSON, (err) => {
+			if (err) {
+				console.error('Error saving to JSON', err)
+				return
+			}
+			console.log('Data saved in JSON:', saveUserToJSON)
+		})
 
 		// insomnia 
 		return reply.send(buffers)
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,9 @@
-// import { users } from './routes/users'
-// import fastify from 'fastify'
-// import fastifyCookie from '@fastify/cookie'
-
 import { app } from './app'
 import { env } from './env'
 
 app
 	.listen({
-		host: '0.0.0.0',
+		host: env.HOST,
 		port: env.PORT,
 	})
 	.then(() => {
